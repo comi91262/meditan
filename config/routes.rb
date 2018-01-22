278bd7b8eb9b0c8ab9de4d6e260849b1ff910392
taba <hgKaguyaToupako4er@gmail.com>
2018-01-22 21:04:43 +0900

    Update route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     get '/questions/search', to: 'questions#search'
+    get '/questions/create', to: 'questions#create'
     resources :questions 
     get '/', to: 'test#index' 
 end
