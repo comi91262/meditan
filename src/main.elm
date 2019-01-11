067172919d984bf52f9b0d2fc40272863f563918
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-01-11 22:30:22 +0900

    WIP

--- src/main.elm
+++ src/main.elm
@@ -1,23 +1,27 @@
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, Attribute, div, input, text, button)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 main =
   Browser.sandbox { init = 0, update = update, view = view }
 
 type Msg = Increment | Decrement
 
-update msg model =
+type InputEvent = Change String
+
+ update msg model =
   case msg of
     Increment ->
-      model + 1
+      model + 2
 
     Decrement ->
-      model - 1
+      model - 1 
 
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+    , button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (String.fromInt model) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
