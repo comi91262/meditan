ed20c7ae613b88249eaf299dfb4088b3f7db73c9
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-02-01 00:22:16 +0900

    Add definition of terms's tables.

---
+++ database/migrations/2019_01_31_144615_create_terms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name_jp', 30)->collation('utf8_general_ci')->comment('日本名');
+            $table->string('name_en', 30);
+            $table->string('category', 10);
+            $table->timestamps();
+            $table->index('category');
+            $table->index('name_jp');
+            $table->index('name_en');
+            $table->unique(['name_jp', 'name_en']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
