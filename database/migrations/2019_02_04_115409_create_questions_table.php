5b4a133f1a411b4484225043b1c60e2795d38d71
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-02-05 13:26:10 +0900

    Question テーブル定義の変更

--- database/migrations/2019_02_04_115409_create_questions_table.php
+++ database/migrations/2019_02_04_115409_create_questions_table.php
@@ -17,12 +17,13 @@ class CreateQuestionsTable extends Migration
 // TODO 何回目の問題セットか
 // TODO user
         Schema::create('questions', function (Blueprint $table) {
-            $table->increments('id');
+            $table->uuid('section');
             $table->integer('number')->comment('問題番号');
             $table->string('question', 100)->comment('問題');
             $table->boolean('success')->default(false)->comment('正解したかどうか');
             $table->dateTime('success_datetime')->nullable()->comment('正解した時の日時');
             $table->timestamps();
+            $table->primary(['section', 'number']);
         });
     }
 
