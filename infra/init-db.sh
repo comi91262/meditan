6575fed2cba68551ebcef570bfd3f255fa9aa2fc
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-02-21 06:03:59 +0900

    テスト用データベースの追加

--- infra/init-db.sh
+++ infra/init-db.sh
@@ -1,2 +1,4 @@
 mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e \
 "CREATE DATABASE IF NOT EXISTS meditan"
+mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e \
+"CREATE DATABASE IF NOT EXISTS meditan_test"
