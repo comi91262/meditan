5a5aa4316d1b6c6415400cdd446ce2db1c934996
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-03-01 00:11:07 +0900

    コミット漏れ

---
+++ app/Models/Term/English.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Term;
+
+use Illuminate\Database\Eloquent\Model;
+
+class English extends Model
+{
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'english_terms';
+}
---
+++ app/Models/Term/Japanese.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Term;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Japanese extends Model
+{
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'japanese_terms';
+}
