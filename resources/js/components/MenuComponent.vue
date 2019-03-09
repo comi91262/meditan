ccaa0dbb693de92dd2ee2e08f7aadc0ea6868b3e
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-03-10 01:57:13 +0900

    メニュー画面改修
    app/User バグを修正

--- resources/js/components/MenuComponent.vue
+++ resources/js/components/MenuComponent.vue
@@ -1,30 +1,30 @@
 <template>
 <el-row class="tac">
-  <el-col :span="12">
+  <el-col>
     <h5>ようこそ</h5>
     <el-menu
-      default-active="2"
       class="el-menu-vertical-demo"
       @open="handleOpen"
-      @close="handleClose">
+      @close="handleClose"
+      @select="jumpPage">
       <el-submenu index="1">
         <template slot="title">
           <span>問題をとく</span>
         </template>
         <el-menu-item-group title="種類">
-          <el-menu-item index="1-1"><a href='/select'>カテゴリテスト</a></el-menu-item>
-          <el-menu-item index="1-2" disabled><a href='/select_condition'>似た単語テスト(テスト運用中)</a></el-menu-item>
-          <el-menu-item index="1-3"><a href='/exam_retry'>やり直しテスト(テスト運用中)</a></el-menu-item>
+          <el-menu-item index="1-1">カテゴリテスト</el-menu-item>
+          <el-menu-item index="1-2" disabled>似た単語テスト(テスト運用中)</el-menu-item>
+          <el-menu-item index="1-3">やり直しテスト</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
       <el-menu-item index="2">
-        <span><a href="/history">テスト履歴へ</a></span>
+        <span>テスト履歴へ</span>
       </el-menu-item>
       <el-menu-item index="3">
-        <span><a href="/list">単語の一覧を表示</a></span>
+        <span>単語の一覧を表示</span>
       </el-menu-item>
       <el-menu-item index="4">
-        <span><a href="/add_term">単語追加(テスト運用中)</a></span>
+        <span>単語追加</span>
       </el-menu-item>
     </el-menu>
   </el-col>
@@ -37,6 +37,27 @@
       handleOpen(key, keyPath) {
       },
       handleClose(key, keyPath) {
+      },
+      jumpPage(key, keyPath) {
+        console.log(key);
+        switch (key) {
+          case "1-1":
+            location.href = '/select';
+            break;
+          case "1-3":
+            location.href = '/exam_retry';
+            break;
+          case "2":
+            location.href = '/history';
+            break;
+          case "3":
+            location.href = '/list';
+            break;
+          case "4":
+            location.href = '/add_term';
+            break;
+
+        }
       }
     }
   }
