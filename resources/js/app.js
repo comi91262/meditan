10c338f219257347e062e28db8e1b7dcdaa52f55
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-03-18 13:36:31 +0900

    vuetify の導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,10 +10,12 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/ja';
-
+ 
+Vue.use(Vuetify);
 Vue.use(VueRouter);
 Vue.use(ElementUI, { locale })
 
