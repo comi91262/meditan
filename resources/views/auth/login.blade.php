3685e3af4387e4ba3fdfcd6fb9513d1896ea5f9e
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-04-02 22:48:25 +0900

    コミット漏れ

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <div id="app">
     <v-app>
+        <header-component></header-component>
         <login-component></login-component>
+        <footer-component></footer-component>
     </v-app>
 </div>
 @endsection
