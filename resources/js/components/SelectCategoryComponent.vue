7330b69504f2c35f2193a8cf6e73bcb0b961cb2a
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-04-03 23:10:25 +0900

    選択画面 を vuetify へ

--- resources/js/components/SelectCategoryComponent.vue
+++ resources/js/components/SelectCategoryComponent.vue
@@ -1,79 +1,83 @@
 <template>
-<el-form ref="form" :model="form" label-width="120px">
-    <h1>選択フォーム</h1>
-    <h2>カテゴリを選択してください</h2>
-    <el-form-item label="">
-        <el-checkbox-group v-model="form.selectedDepartment">
-            <el-checkbox v-for="(department, key) in this.departments"
-                :key="key"
-                :label="department.id">
-                {{department.name}}
-            </el-checkbox>
-        </el-checkbox-group>
-    </el-form-item>
-    <h2>問題文の言語を選択してください</h2>
-    <el-form-item label="">
-        <el-radio-group v-model="form.language">
-            <el-radio label="0">日本語</el-radio>
-            <el-radio label="1">English</el-radio>
-        </el-radio-group>
-    </el-form-item>
-    <el-form-item>
-        <el-button type="primary" @click="onSubmit">Start</el-button>
-        <el-button @click="backHome">トップ画面に戻る</el-button>
-    </el-form-item>
-</el-form>
+  <v-container>
+    <v-form ref="form" lazy-validation>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <h2>カテゴリを選択してください</h2>
+            <v-checkbox v-model="selectedDepartments"
+              v-for="(department, key) in this.departments"
+              :key="key"
+              :label="department.name"
+              :value="department.id"
+            ></v-checkbox>
+        </v-flex>
+
+        <v-flex xs12>
+          <h2>問題文の言語を選択してください</h2>
+          <v-radio-group v-model="language">
+            <v-radio-group>
+              <v-radio label="日本語" :value="0"></v-radio>
+              <v-radio label="English" :value="1"></v-radio>
+            </v-radio-group>
+          </v-radio-group>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-btn color="cyan" @click="onSubmit">Start</v-btn>
+          <v-btn to="/" color="cyan">トップ画面に戻る</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            form: {
-                selectedDepartment: [],
-                language: '',
-                number: 0,
-            },
-            departments: [],
-        }
+  data() {
+    return {
+      selectedDepartments: [],
+      language: 0,
+      number: 0,
+      departments: []
+    };
+  },
+  created: function() {
+    this.getDepartments();
+  },
+  methods: {
+    onSubmit() {
+      axios
+        .post('/api/questions/_kind/category', {
+          categories: this.selectedDepartments,
+          language: this.language
+        })
+        .then(response => {
+          if (response.data.result !== null) {
+            let term = response.data.result;
+            this.$router.push({ path: '/exam' });
+          } else {
+            this.$message.error(response.data.message);
+          }
+        })
+        .catch(error => {
+          if (error.response.status === 401) {
+            this.$message.error('認証エラーです。もう一度ログインください');
+          } else {
+            this.$message.error('通信エラーです。もう一度試してください');
+          }
+        });
     },
-    created: function () {
-        this.getDepartments();
+    backHome() {
+      this.$router.go(-1);
     },
-    methods: {
-        onSubmit() {
-            axios.post('/api/questions/_kind/category', {
-                'categories': this.form.selectedDepartment,
-                'language': this.form.language,
-            })
-            .then(response => {
-                if (response.data.result !== null) {
-                    let term = response.data.result;
-                    this.$router.push({ path: '/exam' });
-                } else {
-                    this.$message.error(response.data.message);
-                }
-            })
-            .catch(error => {
-                if (error.response.status === 401) {
-                    this.$message.error('認証エラーです。もう一度ログインください');
-                } else {
-                    this.$message.error('通信エラーです。もう一度試してください');
-                }
-            })
-        },
-        backHome() {
-            this.$router.go(-1);
-        },
-        getDepartments: function () {
-            axios
-                .get('/api/departments')
-                .then(response => {
-                    this.departments = response.data.departments;
-                })
-                .catch(error => {
-                })
-        },
+    getDepartments: function() {
+      axios
+        .get('/api/departments')
+        .then(response => {
+          this.departments = response.data.departments;
+        })
+        .catch(error => {});
     }
-}
+  }
+};
 </script>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.base')
+@extends('layouts.app')
 
 @section('content')
 <div id="app">
     <v-app>
         <router-view></router-view>
+        <footer-component></footer-component>
     </v-app>
 </div>
 @endsection 
