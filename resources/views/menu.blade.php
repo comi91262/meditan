04561b4d99d86dcebd120dc884e081516e306484
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-04-06 00:29:07 +0900

    メイン画面 header を追加する

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div id="app">
     <v-app>
+        <header-component></header-component>
         <router-view></router-view>
         <footer-component></footer-component>
     </v-app>
