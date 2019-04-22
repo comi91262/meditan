8b84e2408d01e6d01f7f4136dabea306349d9cdd
ykonomi <hgKaguyaToupako4er@gmail.com>
2019-04-22 21:20:27 +0900

    途中経過

--- infra/db/init-db.sh
+++ infra/db/init-db.sh
@@ -1,4 +1,18 @@
-mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e \
+# while :
+# do
+#   if mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "quit" > /dev/null 2>&1 ; then
+#     echo "MySQL master is ready!"
+#     break
+#   else
+#     echo "MySQL master is not ready"
+#   fi
+#   sleep 3
+# done
+# 
+# mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@$SOURCE_IP' IDENTIFIED BY '$MYSQL_REPL_PASSWORD';"
+# mysql -h $MYSQL_HOST -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPL_USER'@'$SOURCE_IP';"
+
+mysql -h db -u root -p$MYSQL_ROOT_PASSWORD -e \
 "CREATE DATABASE IF NOT EXISTS meditan"
-mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD -e \
+mysql -h db -u root -p$MYSQL_ROOT_PASSWORD -e \
 "CREATE DATABASE IF NOT EXISTS meditan_test"
